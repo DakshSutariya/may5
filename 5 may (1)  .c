0f758cdd51e6955f13b4ebf620dd3e2fde2aefc8
DakshSutariya <130463070+DakshSutariya@users.noreply.github.com>
2023-05-05 17:46:06 +0530

    Add files via upload

---
+++ 5 may (1)  .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	char i,n;
+	
+	printf("Enter the value =");
+	scanf("%c",&n);
+	
+	i=n;
+	
+	printf("%c",i+32);
+	
+}
---
+++ 5 may (2)  .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	char i,n;
+	
+	printf("Enter the value =");
+	scanf("%c",&n);
+	
+	i=n;
+	
+	printf("%c",i-32);
+	
+}
+	
+
